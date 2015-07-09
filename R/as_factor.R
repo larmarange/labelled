1c474392fa49c3daf42ef44c545e7ffe803eca23
larmarange <joseph@larmarange.net>
2015-07-09 07:50:16 +0200

    missing_to_NA to FALSE by default and sort_levels added
    "transforming" data should always be explicit

--- R/as_factor.R
+++ R/as_factor.R
@@ -26,23 +26,39 @@ as_factor.default <- function(x, ...) {
 #' @param ordered \code{TRUE} for ordinal factors, \code{FALSE} (default) for nominal factors.
 #' @param missing_to_na Should defined missing values be converted to `NA`?
 #' @param nolabel_to_na Should values with no label be converted to `NA`?
+#' @param sort_levels How the factor levels should be sorted? (see Details)
+#' @param decreasing Sould levels be sorted in decreasing order?
 #' @param quiet Hide messages?
-#' @details The order of the returned factor levels will be the order of value labels, except if some values doesn't have
-#'   a label and \code{nolabel_to_na} is \code{FALSE}. In such case, automatic labels will be created and levels will be
-#'   ordered according to values. To order labels before calling \code{as_factor}, see \code{\link{sort_val_labels}}.
+#' @details
+#'   If some values doesn't have a label, automatic labels will be created, except if
+#'   \code{nolabel_to_na} is \code{TRUE}.
+#'
+#'   If \code{sort_levels == "values"}, the levels will be sorted according to the values of \code{x}.
+#'   If \code{sort_levels == "labels"}, the levels will be sorted according to labels' names.
+#'   If \code{sort_levels == "none"}, the levels will be in the order the value labels are defined
+#'   in \code{x}. If some labels are automatically created, they will be added at the end.
+#'   If \code{sort_levels == "auto"}, \code{sort_levels == "none"} will be used, except if some
+#'   values doesn't have a defined label. In such case,  \code{sort_levels == "values"} will
+#'   be applied.
 #' @examples
 #' v <- labelled(c(1,2,2,2,3,9,1,3,2,NA), c(yes = 1, no = 3, "don't know" = 9), c(F, F, T))
 #' as_factor(v)
 #' as_factor(v, missing_to_na = FALSE, nolabel_to_na = TRUE)
 #' as_factor(v, "p")
+#' as_factor(v, sort_levels = "v")
+#' as_factor(v, sort_levels = "n")
+#' as_factor(v, sort_levels = "l")
 #'
 #' x <- labelled(c("H", "M", "H", "L"), c(low = "L", medium = "M", high = "H"))
 #' as_factor(x, ordered = TRUE)
 #' @export
 as_factor.labelled <-
   function(x, levels = c("labels", "values", "prefixed"),
-           ordered = FALSE, missing_to_na = TRUE, nolabel_to_na = FALSE, quiet = FALSE, ...) {
+           ordered = FALSE, missing_to_na = FALSE, nolabel_to_na = FALSE,
+           sort_levels = c("auto", "none", "labels", "values"), decreasing = FALSE,
+           quiet = FALSE, ...) {
     levels <- match.arg(levels)
+    sort_levels <- match.arg(sort_levels)
     if (missing_to_na)
       x <- missing_to_na(x)
     if (nolabel_to_na)
@@ -50,12 +66,11 @@ as_factor.labelled <-
     labels <- val_labels(x)
     allval <- unique(x)
     allval <- allval[!is.na(allval)]
-    nolabel <- allval[!allval %in% labels]
+    nolabel <- sort(allval[!allval %in% labels])
     # if there are some values with no label
     if (length(nolabel) > 0) {
       names(nolabel) <- nolabel
       levs <- c(labels, nolabel)
-      levs <- sort(levs)
       if (!quiet & levels != "values")
         message(
           gettextf(
@@ -67,6 +82,14 @@ as_factor.labelled <-
     } else {
       levs <- labels
     }
+
+    if (sort_levels == "auto" & length(nolabel) > 0)
+      sort_levels == "values"
+    if (sort_levels == "labels")
+      levs <- levs[order(names(levs), decreasing = decreasing)]
+    else if (sort_levels == "values")
+      levs <- sort(levs, decreasing = decreasing)
+
     if (levels == "labels")
       labs <- names(levs)
     else if (levels == "values")
