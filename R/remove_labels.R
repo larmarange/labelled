755085d0b01d4d1cc1c1dc9a4a19083a63fffe4b
larmarange <joseph@larmarange.net>
2016-06-19 21:12:23 +0200

    removing labels
    - new function remove_var_label
    - new function remove_val_labels
    - zap_labels removed

--- R/remove_labels.R
+++ R/remove_labels.R
@@ -1,6 +1,8 @@
 #' Remove variable and value labels
 #'
-#' This function removes all variable and values labels.
+#' Use \code{remove_var_label} to remove variable label, \code{remove_val_labels}
+#' to remove value labels and \code{remove_labels} to remove both variable and
+#' value labels.
 #'
 #' @param x A vector or a data frame.
 #' @export
@@ -25,9 +27,43 @@ remove_labels.data.frame <- function(x) {
 }
 
 #' @rdname remove_labels
-#' @details
-#' \code{zap_labels} is equivalent to \code{remove_labels}.
 #' @export
-zap_labels <- function(x) {
-  UseMethod("remove_labels")
+remove_var_label <- function(x) {
+  UseMethod("remove_var_label")
+}
+
+
+#' @rdname remove_labels
+#' @export
+remove_var_label.default <- function(x) {
+  var_label(x) <- NULL
+  x
+}
+
+#' @rdname remove_labels
+#' @export
+remove_var_label.data.frame <- function(x) {
+  x[] <- lapply(x, remove_var_label)
+  x
+}
+
+#' @rdname remove_labels
+#' @export
+remove_val_labels <- function(x) {
+  UseMethod("remove_val_labels")
+}
+
+
+#' @rdname remove_labels
+#' @export
+remove_val_labels.default <- function(x) {
+  val_labels(x) <- NULL
+  x
+}
+
+#' @rdname remove_labels
+#' @export
+remove_val_labels.data.frame <- function(x) {
+  x[] <- lapply(x, remove_val_labels)
+  x
 }
