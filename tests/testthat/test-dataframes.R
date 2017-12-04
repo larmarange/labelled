3c60f9a7b944ce24855430d89de7f92a255c41dd
Michal Bojanowski <michal2992@gmail.com>
2017-12-04 13:55:01 +0100

    Added some tests for #20

---
+++ tests/testthat/test-dataframes.R
@@ -0,0 +1,17 @@
+context("Creating data frames with labelled vars")
+
+test_that("data.frame variables are named correctly", {
+  d <- data.frame(
+    lab = labelled(1:5, c(a=1, b=2)),
+    nolab = 1:5
+  )
+  expect_equal(names(d), c("lab", "nolab"))
+})
+
+test_that("tibble variables are named correctly", {
+    d <- dplyr::data_frame(
+    lab = labelled(1:5, c(a=1, b=2)),
+    nolab = 1:5
+  )
+  expect_equal(names(d), c("lab", "nolab"))
+})
