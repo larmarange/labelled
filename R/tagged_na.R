f8f16df302df8248e7e1f0e7c28be69fd11cfa9b
Joseph <joseph@larmarange.net>
2021-04-05 13:25:46 +0200

    draft of dupplicated_tagged_na and unique_tagged_na

--- R/tagged_na.R
+++ R/tagged_na.R
@@ -17,3 +17,24 @@ haven::format_tagged_na
 #' @importFrom haven print_tagged_na
 #' @export
 haven::print_tagged_na
+
+#' @export
+duplicated_tagged_na <- function(x, fromLast = FALSE) {
+  if (!is.double(x))
+    return(duplicated(x, fromLast = fromLast))
+
+  res <- duplicated(x, fromLast = fromLast, incomparables = NA)
+
+  if (anyNA(x))
+    res[is.na(x)] <- duplicated(
+      format_tagged_na(x[is.na(x)]),
+      fromLast = fromLast
+    )
+
+  res
+}
+
+#' @export
+unique_tagged_na <- function(x, fromLast = FALSE) {
+  x[!duplicated_tagged_na(x, fromLast = fromLast)]
+}
