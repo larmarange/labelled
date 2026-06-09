66f693fe8c5cdc43653d44296c7895fb9a5711cd
Joseph Larmarange <joseph@larmarange.net>
2026-06-09 10:35:13 +0200

    lintr::lint_package()

--- tests/testthat/test-labelled.r
+++ tests/testthat/test-labelled.r
@@ -809,7 +809,7 @@ test_that("set_value_labels .labels + .overwrite on data frames", {
   expect_equal(val_labels(df2$s2), c(Yes = 1, No = 2))
   expect_equal(val_labels(df2$x), c(low = 1))
 
-  # .overwrite = TRUE
+  # checking with .overwrite = TRUE
   df2 <- set_value_labels(
     df,
     .labels = list(s1 = c(Male2 = "M"), s2 = c(Maybe = 3), x = c(low = 1)),
